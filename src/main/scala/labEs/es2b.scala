5a7cd8b37d9917c4b4f6f67da29342d5e3af4dd6
Andrea Negri <andrea.negri4@studio.unibo.it>
2023-03-28 11:54:47 +0200

    Completato es 2b

---
+++ src/main/scala/labEs/es2b.scala
@@ -0,0 +1,29 @@
+package labEs
+
+object es2b extends App:
+
+  println("Test predicate")
+
+  val p1: Int => Int => Int => Boolean = x => y => z => x <= y && y == z
+  val p2: (Int, Int, Int) => Boolean = (x, y, z) =>  x <= y && y == z
+  def p3(x: Int)(y: Int)(z: Int): Boolean = x <= y && y == z
+  def p4(x: Int, y: Int, z: Int): Boolean = x <= y && y == z
+
+  println(p1(10)(15)(15))
+  println(p2(10, 15, 15))
+  println(p1(20)(15)(15))
+  println(p2(20, 15, 15))
+
+  println(p3(10)(15)(15))
+  println(p4(10, 15, 15))
+  println(p3(20)(15)(15))
+  println(p4(20, 15, 15))
+
+  println("Test compose")
+
+  def compose(f: Int => Int, g: Int => Int): Int => Int = x => f(g(x))
+  println(compose(_ - 1, _ * 2)(5))
+
+  def composeGen[X](f: X => X, g: X => X): X => X = x => f(g(x))
+
+
